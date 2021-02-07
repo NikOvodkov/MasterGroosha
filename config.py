704cf63024d34363a97f75b4798d3f57818bd953
NikOvodkov <nikovodkov@ya.ru>
2021-02-07 14:53:50 +0300

    1st commit

---
+++ config.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+database_name = 'music.db'  # Файл с базой данных
+shelve_name = 'shelve.db'  # Файл с хранилищем
+telegram_server_key = "1405488357:AAFErDH-aumdXDQgmxlm6HrSCMwPEohfJ-g"  # for SERVER
+telegram_pc_key = "1426953978:AAHjeRBYJZPpOJfGNeq6jjSrjUmj9jeoUQI"  # for PC
+flask_path = "NikOvodkov.pythonanywhere.com/home/NikOvodkov/mysite/flask_app.py"
+secret = "secret"
+MY_ID = "497725007"
+DB_FILENAME = "testbd.db"
