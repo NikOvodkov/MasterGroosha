bbbbf24ba8be4b3189007f052bdf04a302efe5bf
NikOvodkov <78206433+NikOvodkov@users.noreply.github.com>
2024-08-06 18:47:23 +0300

    Update config.py fake key

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 database_name = 'music.db'  # Файл с базой данных
 shelve_name = 'shelve.db'  # Файл с хранилищем
-telegram_server_key = "1405488357:AAFErDH-aumdXDQgmxlm6HrSCMwPEohfJ-g"  # for SERVER
-telegram_pc_key = "1426953978:AAHjeRBYJZPpOJfGNeq6jjSrjUmj9jeoUQI"  # for PC
+telegram_server_key = "1405488357:AAFErDH-aumdXDQgmxlm6HrSCMwPEoohfJ-g"  # for SERVER fake key
+telegram_pc_key = "1426953978:AAHjeRBYJZPpOJfGNeq6jjSrjUmj9joeoUQI"  # for PC fake key
 flask_path = "NikOvodkov.pythonanywhere.com/home/NikOvodkov/mysite/flask_app.py"
 secret = "secret"
 MY_ID = "497725007"
